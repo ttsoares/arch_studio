d70e4f7270e89b35e2b57c037dda5f83f9d8f0fb
Thomas T. Soares <ttsoares@orion.ufrgs.br>
2023-11-28 18:11:33 -0300

    Footer OK

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -12,28 +12,28 @@ import Buttom from "@/components/Buttom";
 
 const Footer = () => {
   return (
-    <div className="flex  flex-col md:flex-row md:w-full lg:w-[77%] h-40 mx-auto mt-32 items-center justify-between relative">
-      <div className="bg-cus_vl_gray flex flex-col md:flex-row items-center justify-start w-[80%]">
+    <div className="flex  flex-col md:flex-row w-full lg:w-[77%] h-40 mx-auto mt-32 items-center justify-between relative">
+      <div className="bg-cus_l_gray flex flex-col md:flex-row items-center justify-start w-full md:w-[80%] relative">
         <Link href="/">
-          <div className="w-40 h-40 bg-black flex">
+          <div className="absolute -top-16 left-32 md:static w-28 h-28 lg:w-40 lg:h-40 bg-black flex">
             <Image
               src={Logo}
               alt="Logo Arch"
-              className="w-20 h-20 invert m-auto self-center"
+              className="w-20 lg:w-20  invert m-auto self-center"
             />
           </div>
         </Link>
 
-        <div className="flex-1 h-20 flex flex-col md:flex-row items-center justify-start ml-8 text-gray-500">
+        <div className="flex-1 h-20 flex flex-col md:flex-row items-center justify-start md:ml-8 text-gray-500 mt-16 md:mt-0">
           {MENU_ITENS.map((item, index) => (
-            <div key={index} className="px-8">
+            <div key={index} className="py-6 md:py-0 md:px-4 lg:px-8">
               <Link href={item.link}>{item.text}</Link>
             </div>
           ))}
         </div>
-        <div className="md:hidden top-10 -right-1 z-10">
+        <div className="md:hidden  top-10 -right-1 z-10">
           <Buttom link="portfolio">
-            <p>See Out Portfolio</p>
+            <p className="text-base">See Out Portfolio</p>
           </Buttom>
         </div>
       </div>
